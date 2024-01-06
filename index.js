1721aea796187398c9256f10b588744e181a40f2
emanshah466 <emanshahshah466@gmail.com>
2024-01-06 14:39:53 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+my.js
+
+function tgl()
+{
+var t = document.getElementById("myBtn");
+if(t.value=="ON"){
+      t.value="OFF";}
+else if(t.value=="OFF"){
+      t.value="ON";}
+}
---
+++ style.js
@@ -0,0 +1,11 @@
+
+
+function howManyYears() {
+
+	var Years = document.getElementById("Years").value;
+	var result = Years * 365;
+	document.getElementById("Days").value = result;
+
+}
+
+
